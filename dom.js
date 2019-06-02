f3d7afdb3fd69a22484d8c23322fdf6d075699fb
Lucas0612 <51293565+Lucas0612@users.noreply.github.com>
2019-06-02 16:57:01 -0300

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,3 +1,36 @@
+$(document).ready(function() {
+$('#botao').click(function() {
+    // alert($('#pesquisa').val());
 
+    if($('#pesquisa').val() == "Farinha de Trigo - 50 Km"){
+        
+        window.location.assign("farinha%20cristal.html");
+    }
+    
 
+    if($('#pesquisa').val() == "Farinha de Trigo - 25 Km"){
+        
+        window.location.assign("farinha%20nordeste.html");
+    }
+
+    if($('#pesquisa').val() == "Farinha de Trigo - 230 Km"){
+        
+        window.location.assign("farinha%20criciuma.html");
+    }
+
+    if($('#pesquisa').val() == "Farinha de Trigo - 700 Km"){
+        
+        window.location.assign("farinha%20saopaulo.html");
+    }
+    
+    if($('#pesquisa').val() == "Farinha de Trigo - 3189 Km" || $('#pesquisa').val()>700){
+        
+        window.location.assign("farinha%20amapa.html");
+    }
+    
+    
+}) ;   
+ 
+
+});
 
